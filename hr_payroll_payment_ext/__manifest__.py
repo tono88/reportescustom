9d45727753a95cb8b77f159e24f99a440f68192b
tono88 <estuarsandoval@gmail.com>
2025-09-02 20:29:16 -0600

    Actualización de bancos y nomina

---
+++ hr_payroll_payment_ext/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Payroll Payments from Batch (Community)",
+    "summary": "Create and track payments (including checks) directly from payroll batches",
+    "version": "18.0.1.0.0",
+    "category": "Human Resources/Payroll",
+    "depends": ["hr_payroll", "account", "hr"],
+    "author": "Blockera Bustamante / ChatGPT",
+    "website": "https://example.com",
+    "license": "LGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/hr_payroll_payment_views.xml"
+    ],
+    "installable": true,
+    "application": false
+}
