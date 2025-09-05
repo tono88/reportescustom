964c5d5586afee15bc24df45fdef199e4e9db677
tono88 <estuarsandoval@gmail.com>
2025-09-04 22:03:12 -0600

    Update account_payment.py

--- account_payment_instant_reconcile/models/account_payment.py
+++ account_payment_instant_reconcile/models/account_payment.py
@@ -1,48 +1,48 @@
 # -*- coding: utf-8 -*-
 from odoo import api, models
 
+RECEIVABLE_TYPES = ('asset_receivable',)
+PAYABLE_TYPES = ('liability_payable',)
+
 class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     def action_post(self):
         res = super().action_post()
-        # Reconciliation logic: for each posted payment, if the journal flag is enabled,
-        # reconcile receivable/payable lines immediately with open items of the partner.
         for payment in self:
             journal = payment.journal_id
             if not journal.instant_reconcile_on_post:
                 continue
 
-            # Only attempt for posted moves
             move = payment.move_id
             if not move or move.state != "posted":
                 continue
 
-            company = payment.company_id
             partner = payment.partner_id.commercial_partner_id
 
-            # Collect payment lines on receivable/payable that are not reconciled
+            # Lines on receivable/payable not reconciled
             pay_lines = move.line_ids.filtered(
-                lambda l: l.account_id.internal_type in ('receivable', 'payable') and not l.reconciled
+                lambda l: not l.reconciled and l.account_id and l.account_id.account_type in (RECEIVABLE_TYPES + PAYABLE_TYPES)
             )
             if not pay_lines:
                 continue
 
-            # For each payment line, find opposite open items for the same partner/account/company
             for pl in pay_lines:
-                sign = 1 if pl.balance > 0 else -1
+                # Build domain for opposite open items for same partner/account
                 domain = [
-                    ('company_id', '=', company.id),
+                    ('company_id', '=', pl.company_id.id),
                     ('account_id', '=', pl.account_id.id),
                     ('partner_id', '=', partner.id),
                     ('reconciled', '=', False),
                     ('move_id.state', '=', 'posted'),
-                    # opposite sign (allow tiny rounding)
-                    ('balance', '<', 0) if pl.balance > 0 else ('balance', '>', 0),
+                    ('id', '!=', pl.id),
                 ]
-                counterpart_lines = self.env['account.move.line'].search(domain, limit=200)
+                # Opposite sign criterion using balance
+                if pl.balance > 0:
+                    domain.append(('balance', '<', 0))
+                elif pl.balance < 0:
+                    domain.append(('balance', '>', 0))
+                counterpart_lines = self.env['account.move.line'].search(domain, limit=500)
                 if counterpart_lines:
-                    # Try to reconcile payment line with the found counterpart lines
-                    # Odoo's reconcile supports partials if amounts don't match exactly.
                     (pl + counterpart_lines).with_context(skip_full_reconcile_check=False).reconcile()
         return res
