8d3fdb2e1d5d739c429d16e6720236f9f6cba075
tono88 <estuarsandoval@gmail.com>
2025-10-07 15:26:27 -0600

    .

---
+++ l10n_gt_fel_megaprint_qr_footer_fix_v4/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "GT FEL Megaprint - QR en Pie de Factura (HQ Logo)",
+    "summary": "Logo FEL en alta resolución + QR al verificador en pie del reporte Megaprint.",
+    "version": "18.0.1.3",
+    "license": "LGPL-3",
+    "author": "Estuardo & ChatGPT",
+    "depends": ["l10n_gt_fel_megaprint_report"],
+    "data": ["reports/qr_footer.xml"],
+    "installable": True,
+    "application": False
+}
---
+++ l10n_gt_header_phoneonly_layout_v3/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "GT Header (teléfono debajo del logo) aplicado a Megaprint - v3",
+    "summary": "Header apilado vertical: logo y debajo 'Teléfono: ...'. Parchea la factura Megaprint para usar este layout.",
+    "version": "18.0.1.2",
+    "license": "LGPL-3",
+    "author": "Estuardo & ChatGPT",
+    "depends": ["l10n_gt_fel_megaprint_report", "web"],
+    "data": ["reports/layout_phone.xml", "reports/patch_invoice_call.xml"],
+    "installable": True,
+    "application": False
+}
