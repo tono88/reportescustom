3a0789d95c373922d9c9cdf2b551324f857a1c7c
tono88 <estuarsandoval@gmail.com>
2025-10-07 14:01:34 -0600

    .

---
+++ l10n_gt_fel_megaprint_qr_footer/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "GT FEL Megaprint - QR en Pie de Factura",
+    "summary": "Extiende el reporte de Megaprint para agregar logo FEL y QR al pie.",
+    "version": "18.0.1.0",
+    "license": "LGPL-3",
+    "author": "Estuardo & ChatGPT",
+    "depends": ["l10n_gt_fel_megaprint_report"],
+    "data": [
+        "reports/qr_footer.xml"
+    ],
+    "installable": true,
+    "application": false
+}
