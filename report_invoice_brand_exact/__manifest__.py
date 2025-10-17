827ee2c45e779031115f7c4f360288289cfccfac
tono88 <estuarsandoval@gmail.com>
2025-10-16 22:35:23 -0600

    .

---
+++ report_invoice_brand_exact/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Invoice Report Brand Exact",
+    "version": "18.0.1.0.0",
+    "summary": "Precisely replace the hardcoded brand div in Megaprint invoice header.",
+    "author": "ChatGPT",
+    "license": "LGPL-3",
+    "depends": ["account", "l10n_gt_fel_megaprint_report"],
+    "data": ["views/report_invoice_brand_exact.xml"],
+    "installable": True,
+    "application": False
+}
