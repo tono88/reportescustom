270890ac1c92d068b86c00f028289df0838fd94f
tono88 <estuarsandoval@gmail.com>
2025-10-27 16:37:11 -0600

    .

---
+++ l10n_gt_check_printing_payee_center_safe/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Guatemala - Check Printing (Payee Patch + shift + inline cents)",
+    "summary": "Beneficiario entre fecha y monto; fecha/beneficiario/monto +20mm; centavos en misma línea (seguro en QWeb).",
+    "version": "18.0.1.0.4",
+    "author": "Estuardo Sandoval & ChatGPT",
+    "website": "https://example.com",
+    "category": "Accounting/Localizations",
+    "license": "LGPL-3",
+    "depends": ["l10n_gt_check_printing"],
+    "data": ["reports/report_check_payee_center.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
