b0eb856712179275ab967628946069e3f1b8e965
tono88 <estuarsandoval@gmail.com>
2025-10-27 17:44:24 -0600

    .

---
+++ l10n_gt_check_printing_forceleft_v8/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Guatemala - Check Printing (Force Left + Inline Cents) v8",
+    "summary": "Fija left:38mm para fecha y monto; beneficiario en 38mm; centavos inline por CSS con !important.",
+    "version": "18.0.1.1.1",
+    "author": "Estuardo Sandoval & ChatGPT",
+    "website": "https://example.com",
+    "category": "Accounting/Localizations",
+    "license": "LGPL-3",
+    "depends": ["l10n_gt_check_printing"],
+    "data": ["reports/report_check_payee_center.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
