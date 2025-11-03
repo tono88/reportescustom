457a1261bb2f8d2109cb888a02587dd3df65e0c0
tono88 <estuarsandoval@gmail.com>
2025-11-03 15:14:18 -0600

    .

---
+++ account_payment_partner_counterpart_v101/models/account_payment.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+
+class AccountPayment(models.Model):
+    _inherit = "account.payment"
+
+    # Odoo 18 (y algunos módulos) pasan argumentos extra (force_balance, existing_lines).
+    # Hacemos la firma 100% compatible y propagamos a super() los mismos argumentos.
+    def _prepare_move_line_default_vals(self, write_off_line_vals=None, force_balance=None, existing_lines=None):
+        vals_list = super()._prepare_move_line_default_vals(
+            write_off_line_vals=write_off_line_vals,
+            force_balance=force_balance,
+            existing_lines=existing_lines,
+        )
+
+        journal = self.journal_id
+        if journal.type not in ("bank", "cash"):
+            return vals_list
+
+        partner = self.partner_id.commercial_partner_id
+        if not partner:
+            return vals_list
+
+        liquidity_account = journal.default_account_id
+        if not liquidity_account:
+            return vals_list
+
+        partner_account = (
+            partner.property_account_receivable_id
+            if self.partner_type == "customer"
+            else partner.property_account_payable_id
+        )
+        if not partner_account:
+            return vals_list
+
+        # Forzar que la contrapartida sea AR/AP y dejar la línea de liquidez limpia de partner
+        for v in vals_list:
+            if v.get("account_id") == liquidity_account.id:
+                v["partner_id"] = False
+            else:
+                v["account_id"] = partner_account.id
+                v["partner_id"] = partner.id
+
+        return vals_list
