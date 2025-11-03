518bb64586db70256759da6c6b2d763e2b9befd7
tono88 <estuarsandoval@gmail.com>
2025-11-03 16:13:42 -0600

    .

---
+++ account_payment_instant_reconcile_v2/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Instant Reconcile & Fix Liquidity-Only Payment",
+    "summary": "Reconciles payments on post and repairs liquidity-only entries (bank↔bank) by using partner AR/AP.",
+    "version": "18.0.1.1.0",
+    "category": "Accounting",
+    "license": "LGPL-3",
+    "author": "Estuardo & ChatGPT",
+    "website": "https://example.com",
+    "depends": ["account"],
+    "data": [
+        "views/account_journal_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
